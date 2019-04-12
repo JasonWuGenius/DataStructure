6cc4d5070675155fe40bf005cf394b1719a4f842
JasonWuGenius <2480629967@qq.com>
2019-04-12 23:41:49 +0800

    20190412

--- Leetcode/test.java
+++ Leetcode/test.java
@@ -1,10 +1,63 @@
-package Leetcode;
-
+import java.util.Scanner;
 public class test {
-	int index = 1;
-	int[] count = new int[256];
-	public void insert(char ch){
-		if(count[ch]  == 0)
-	}
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        double[] rand = new double[n+1];
+        rand[0] = 0;
+        for(int i=1; i<=n; i++){
+        	rand[i] = in.nextDouble();
+        }
+        double res1 = 0;
+        double res2 = 0;
+        int count = 0;
+        int index = 1;
+        while(res1 < 0.5 && res2 < 0.5){
+        	if(index % 2 == 1){
+        		int i=1;
+        		for(; i<=n; i++){
+        			if(i % 2 == 1){
+        				res1 += rand[i];
+        			}
+        			if(i % 2 != 1){
+        				res2 += rand[i];
+        			}
+        			if(count++ == 100){
+        				break;
+        			}
+        		}
+        	}
+        	else{
+        		if(n % 2 == 1){
+        			int i=1;
+        			for(; i<=n; i++){
+        				if(i % 2 == 1){
+        					res2 += rand[i];
+        				}
+        				if(i % 2 != 1){
+        					res1 += rand[i];
+        				}
+        				if(count++ == 100){
+        					break;
+        				}
+        			}
+        		}
+        		else{
+        			int i=1;
+        			for(; i<=n; i++){
+        				if(i % 2 == 1){
+        					res1 += rand[i];
+        				}
+        				if(i % 2 != 1){
+        					res2 += rand[i];
+        				}
+        				if(count++ == 100){
+        					break;
+        				}
+        			}
+        		}
+        	}
+        }
+        System.out.printf("%.4f",res1);
+    }
 }
