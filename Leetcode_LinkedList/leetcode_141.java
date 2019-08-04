465ee4748cdb082702520454c4bace09db7b026e
JasonWuGenius <2480629967@qq.com>
2019-08-04 17:03:59 +0800

    20190804

--- Leetcode_LinkedList/leetcode_141.java
+++ Leetcode_LinkedList/leetcode_141.java
@@ -1,34 +1,37 @@
 package Leetcode_LinkedList;
 
+/*
+ * 环形链表
+ * 给定一个链表，判断链表中是否有环。
+	为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+	示例 1：
+	输入：head = [3,2,0,-4], pos = 1
+	输出：true
+	解释：链表中有一个环，其尾部连接到第二个节点。
+	
+	思路
+	使用两个指针，一个快指针，一个慢指针；
+	快的每次走两步，慢的每次走一步，如果有环，那么快的肯定可以追上慢点，两者相等
+ */
 public class leetcode_141 {
-	/*
-	 * 环形链表
-	 * 给定一个链表，判断链表中是否有环。
-		为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
-		示例 1：
-		输入：head = [3,2,0,-4], pos = 1
-		输出：true
-		解释：链表中有一个环，其尾部连接到第二个节点。
-		
-		思路
-		使用两个指针，一个快指针，一个慢指针；
-		快的每次走两步，慢的每次走一步，如果有环，那么快的肯定可以追上慢点，两者相等
-	 */
 	public static boolean hasCycle(ListNode head){
-		if(head == null || head.next == null)
+		if(head == null || head.next == null){
 			return false;
-		ListNode fast = head.next;
-		ListNode slow = head;
-		while(fast != null && slow != null){
-			if(fast == slow)
-				return true;
-			slow = slow.next;
-			if(fast.next != null)
-				fast = fast.next.next;
-			else
-				return false;
 		}
-		return false;
+		ListNode fast = head.next.next;
+		ListNode slow = head.next;
+		while(true){
+			if(fast == null || fast.next == null){
+				return false;
+			}
+			else if(fast == slow || fast.next == slow){
+				return true;
+			}
+			else{
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+		}
 	}
 	public static void main(String[] args) {
 		ListNode node1 = new ListNode(3);
--- Leetcode_LinkedList/leetcode_203.java
+++ Leetcode_LinkedList/leetcode_203.java
@@ -1,19 +1,19 @@
 package Leetcode_LinkedList;
 
-public class leetcode_203 {
-	/*
-	 *  删除链表中等于给定值 val 的所有节点。
-		示例:
-		输入: 1->2->6->3->4->5->6, val = 6
-		输出: 1->2->3->4->5
+/*
+ *  删除链表中等于给定值 val 的所有节点。
+	示例:
+	输入: 1->2->6->3->4->5->6, val = 6
+	输出: 1->2->3->4->5
 
-		思路：
-		首先是删除开头的节点与val值相等的节点；
-		然后是删除中间的与val相等的节点，采取之前的这道题的解法每天一道leetcode237-删除链表中的节点，
-		就是交换当前节点与下一个节点的val值，然后把下一个节点删除就行；
-		最后处理最后的一个节点。
-	 */
-	//现在表头添加一个header结点
+	思路：
+	首先是删除开头的节点与val值相等的节点；
+	然后是删除中间的与val相等的节点，采取之前的这道题的解法每天一道leetcode237-删除链表中的节点，
+	就是交换当前节点与下一个节点的val值，然后把下一个节点删除就行；
+	最后处理最后的一个节点。
+ */
+//现在表头添加一个header结点
+public class leetcode_203 {
 	public static ListNode removeElements(ListNode head, int val){
 		ListNode header = new ListNode(-1);
 		header.next = head;
