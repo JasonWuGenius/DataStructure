bf4b20748dca60c1dac3fbac95cc145a5419fbb7
JasonWuGenius <2480629967@qq.com>
2019-08-04 17:14:25 +0800

    20190804

--- Leetcode_String/leetcode_387.java
+++ Leetcode_String/leetcode_387.java
@@ -3,16 +3,16 @@ package Leetcode_String;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+	案例:
+	s = "leetcode"
+	返回 0.
+	s = "loveleetcode",
+	返回 2.
+	注意事项：您可以假定该字符串只包含小写字母。
+ */
 public class leetcode_387 {
-	/*
-	 * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
-		案例:
-		s = "leetcode"
-		返回 0.
-		s = "loveleetcode",
-		返回 2.
-		注意事项：您可以假定该字符串只包含小写字母。
-	 */
 	//方法一：map
 	public static int findUniqChar1(String s){
 		Map<Character, Integer> map = new HashMap<>(26);
