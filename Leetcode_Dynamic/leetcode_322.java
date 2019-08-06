03fd70aa4218bf8dc03c398bf7c2f84038589b1a
JasonWuGenius <2480629967@qq.com>
2019-08-06 11:01:32 +0800

    20190806

--- Leetcode_Dynamic/leetcode_322.java
+++ Leetcode_Dynamic/leetcode_322.java
@@ -62,13 +62,15 @@ public class leetcode_322 {
 
 	public static int coinChange1(int[] coins, int amount){
 		int[] dp = new int[amount+1];
+		dp[0] = 0;
 		for(int i=1; i<amount+1; i++){
 			dp[i] = amount+1;
 		}
-		dp[0]=0;
 		for(int i=1; i<amount+1; i++){
 			for(int j=0; j<coins.length; j++){
-				dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
+				if(i-coins[j]>=0){
+					dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
+				}
 			}
 		}
 		if(dp[amount] == amount+1){
@@ -79,7 +81,7 @@ public class leetcode_322 {
 	public static void main(String[] args) {
 		int[] coins = {1,2,5};
 		int amount = 11;
-		System.out.println("The solution: "+coinChange(coins, amount));
+		System.out.println("The solution: "+coinChange1(coins, amount));
 	}
 
 }
